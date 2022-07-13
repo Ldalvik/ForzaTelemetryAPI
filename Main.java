291eda19a34605883d3961bcfe202595b5df77d5
Julian Osborne <subaruforge@gmail.com>
2022-07-13 00:29:51 -0400

    Updated API to use interfaces for callback handlers

---
+++ Main.java
@@ -0,0 +1,65 @@
+package root.forza.telemetry;
+
+import java.net.DatagramPacket;
+import java.net.UnknownHostException;
+
+public class Main implements ForzaInterface {
+    public static void main(String[] args) throws UnknownHostException {
+        new ForzaTelemetryBuilder(/*Optional, default 5300*/)
+                .addListener(new Main()) //Add listener, aka this main class
+                .getThread() //Get created thread, will need to be set in your class if interrupts are needed
+                .start(); //Start thread (this is being called on a Thread)
+
+        //Bringing the data stream thread into your scope for accessing.
+        /*
+          ForzaTelemetryBuilder builder = new ForzaTelemetryBuilder()
+                  .addListener(new Main());
+          Thread dataStreamThread = builder.getThread();
+          dataStreamThread.start();
+         */
+
+        System.out.println("You will be connecting to: "
+            + ForzaInterface.getDeviceIp());
+    }
+
+    @Override
+    public void onDataReceived(ForzaTelemetryApi api) {
+        System.out.println("X Position: " + api.getPositionX());
+    }
+
+    @Override
+    public void onConnected(ForzaTelemetryApi api, DatagramPacket packet) {
+        System.out.println("Connected to: "
+                + packet.getSocketAddress());
+
+        System.out.println("Car Ordinal: " + api.getOrdinal());
+    }
+
+    @Override
+    public void onDisconnected(DatagramPacket packet) {
+        System.out.println("Disconnected from:  "
+                + packet.getSocketAddress());
+    }
+
+    @Override
+    public void onGamePaused() {
+        System.out.println("Game paused.");
+    }
+
+    @Override
+    public void onGameUnpaused() {
+        System.out.println("Game unpaused.");
+    }
+
+    @Override
+    public void onCarChanged(ForzaTelemetryApi api, VehicleData data) {
+        System.out.println("Vehicle changed.");
+        System.out.println("Car Name: " + data.getCarName());
+        System.out.println("Car Class: " + data.getCarClass());
+        System.out.println("Performance Index (PI): " + data.getPerformanceIndex());
+        System.out.println("Drivetrain: " + data.getDrivetrain());
+        System.out.println("Number of Cylinders: " + data.getNumOfCylinders());
+        System.out.println("Car Type: " + data.getCarType());
+        System.out.println("Ordinal: " + data.getOrdinal());
+    }
+}
